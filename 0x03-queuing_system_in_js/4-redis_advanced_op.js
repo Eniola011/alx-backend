421f834c8997c32e65a93924c1846a61e4885fb5
Eniola011 <yettyagbalu@gmail.com>
2024-06-11 11:49:48 +0000

    Task 4:Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,34 @@
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error('Redis client not connected to the server:', err);
+});
+
+function createHash() {
+  client.hset('HolbertonSchools', 'Portland', 50, print);
+  client.hset('HolbertonSchools', 'Seattle', 80, print);
+  client.hset('HolbertonSchools', 'New York', 20, print);
+  client.hset('HolbertonSchools', 'Bogota', 20, print);
+  client.hset('HolbertonSchools', 'Cali', 40, print);
+  client.hset('HolbertonSchools', 'Paris', 2, print);
+}
+
+function displayHash() {
+  client.hgetall('HolbertonSchools', (err, obj) => {
+    if (err) {
+      console.error('Error:', err);
+    } else {
+      console.log(obj);
+    }
+  });
+}
+
+// Call the functions
+createHash();
+displayHash();
